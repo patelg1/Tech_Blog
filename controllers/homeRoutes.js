689385aeeaf6ae3eb98ad04d637b8c2dfe089379
Gopal Patel <patelgopalk1@gmail.com>
2021-04-09 23:34:02 -0400

    reinstall npm packages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,7 +49,7 @@ router.get('/post/:id', async (req, res) => {
       });
   
       const post = postData.get({ plain: true });
-      console.log(post)
+      
       res.render('singlepost', {
         post,
         logged_in: req.session.logged_in
--- public/js/addBlogPost.js
+++ public/js/addBlogPost.js
@@ -3,14 +3,14 @@ const newFormHandler = async (event) => {
 
     const title = document.querySelector('#post-title').value.trim();
     const content = document.querySelector('#post-content').value.trim();
-    console.log('before response')
+    
 
     const response = await fetch(`/api/post`, {
         method: 'POST',
         body: JSON.stringify({title, content}),
         headers: {'Content-Type': 'application/json'}
     });
-    console.log(response.ok)
+    
     if (response.ok) {
         document.location.replace('/dashboard');
     }else {
