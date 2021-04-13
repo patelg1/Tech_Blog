4816eda65a0d574dedbce76b716817c86bf933e8
Gopal Patel <patelgopalk1@gmail.com>
2021-04-13 19:40:39 -0400

    Changed model name

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require('./UserModel');
+const Post = require('./PostModel');
+const Comment = require('./CommentModel');
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
